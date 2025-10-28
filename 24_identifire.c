a419d2cb08571f8a3ec44be5199b6c150ec13200
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-10-28 20:00:49 +0530

    10/28

---
+++ 24_identifire.c
@@ -0,0 +1,12 @@
+// example of identifier
+#include <stdio.h>
+
+void main()
+{
+    int _5For;
+    int age;
+    int height;
+    int num1;
+    // int 5hello;  its show error
+    // int float;   its show error
+}
---
+++ 25_constatnt.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+    const int age = 12;
+    printf("age = %d\n", age);
+    // age = 89;
+    printf("age = %d\n", age);
+}
