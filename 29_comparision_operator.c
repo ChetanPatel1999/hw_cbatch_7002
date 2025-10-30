ffb9a3e1cdfc6085836aa8c12b1c9a4291b65abe
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-10-30 20:02:23 +0530

    29/10

---
+++ 29_comparision_operator.c
@@ -0,0 +1,10 @@
+// example of comparision /relational operator
+#include <stdio.h>
+void main()
+{
+    int a, b, c;
+    a = 56;
+    b = 88;
+    c = a != b;
+    printf("ans = %d", c);
+}
---
+++ 30_compound_assignment.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int a = 12, b = 7;
+    printf("a = %d\n", a); // 12
+    printf("b = %d\n", b); // 7
+    // a += b;
+    // a -= b;
+    // a += 9;
+    // b += b;
+    // b *= a;
+    // a %= b;
+    // b /= a;
+    // a /= b;
+    a = 12;
+    b = 5;
+    // a += 1;
+    // a = +b;
+    // a = -b;
+    // a -= b;
+    printf("a = %d\n", a); // 5
+    printf("b = %d\n", b); // 5
+}
